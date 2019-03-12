a727b34f317f524ff4359ac472c8ed731a66e727
Halofan240 <a.pytlik@comcast.net>
2019-03-12 00:34:43 -0600

    Lab 4: Have plot 1 and 2 set up. Need to fix the interpolaing. Close but not close enough. Fix the errors involving some temps going straight to 0 and back.
    Added Comments to code

--- lab4/alexander_pytlik_hw4.py
+++ lab4/alexander_pytlik_hw4.py
@@ -34,10 +34,12 @@ def read_wx_data(wx_file, harbor_data):
         for line in file:
             recs = line.split(',')  # Splits the line into columns
 
+            # Checks to see if its the initial time
             if is_init_time:
                 init_time = recs[1]
                 is_init_time = False
 
+            # gets an converts the time to hours, min, secs
             delta_t = dt.strptime(recs[1], '%H:%M:%S') - dt.strptime(init_time, '%H:%M:%S')  # get delta time
             harbor_data['wx_times'].append(float(delta_t.total_seconds() / 3600))  # convert to hours
             harbor_data["wx_temperatures"].append(float(recs[3]))
@@ -70,7 +72,7 @@ def read_gps_data(gps_file, harbor_data):
                 init_time = ":".join(recs[0:3])
                 is_init_time = False
 
-            # get delta time
+            # gets an converts the time to hours, min, secs
             delta_t = dt.strptime(":".join(recs[0:3]), '%H:%M:%S') - dt.strptime(init_time, '%H:%M:%S')
             harbor_data['gps_times'].append(float(delta_t.total_seconds() / 3600))  # convert to hours
             harbor_data["gps_altitude"].append(int(recs[6]))
@@ -92,16 +94,18 @@ def interpolate_wx_from_gps(harbor_data):
     harbor_data["alt_down"] = []
     harbor_data["temp_down"] = []
 
+    # Creates a linear line of times from 0 to max gps time then interp1d altitude into it
     x = np.linspace(0, harbor_data["gps_times"][-1], num=len(harbor_data["wx_times"]))
     inter_altitude = ip.interp1d(harbor_data["gps_times"], harbor_data["gps_altitude"], fill_value="extrapolate")
     wx_alt = inter_altitude(x)
 
+    # initial variables for below
     current_alt = 0
     current_temp = 0
     pos = 0
 
+    # Loops through wx_alt and separates the values into increasing altitude and decreasing altitude
     for i in wx_alt:
-        h = harbor_data["wx_temperatures"][pos]
         if i > current_alt:
             harbor_data["alt_up"].append(i)
             harbor_data["temp_up"].append(harbor_data["wx_temperatures"][pos])
@@ -114,7 +118,10 @@ def interpolate_wx_from_gps(harbor_data):
             current_alt = i
         pos += 1
 
-    h = 3
+    """
+    Below is my attempt at trying to correct the graph with correct values
+    """
+
     # i_count = 0
     # j_count = 0
     # current_alt = 0
@@ -152,9 +159,15 @@ def interpolate_wx_from_gps(harbor_data):
 
 
 def match_time(harbor_data):
+    """
+    Use to match the temperature time with the gps time
+    :param harbor_data: A dictionary to collect data.
+    :return:
+    """
     gps_max_hour = harbor_data["gps_times"][len(harbor_data["gps_times"]) - 1]
     count = 0
 
+    # loops through and sets the the temperature time to match gps time
     for t in harbor_data["wx_times"]:
         if t > gps_max_hour:
             del harbor_data["wx_times"][count:]
@@ -169,6 +182,7 @@ def plot_figs(harbor_data):
     :return: nothing
     """
 
+    # Contains the graph for the first part of the assignment
     fig1, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex="all")
     ax1.set_title("Harbor Flight Data")
     ax1.plot(harbor_data["wx_times"], harbor_data["wx_temperatures"], "b-")
@@ -180,28 +194,35 @@ def plot_figs(harbor_data):
     ax2.plot(harbor_data["gps_times"], harbor_data["gps_altitude"], "b-")
     ax2.set_xlim(0, 2.5)
 
+    # Contains the graph for the second part of assignment
     fig2, (ax3, ax4) = plt.subplots(nrows=1, ncols=2)
     ax3.set_ylabel("Altitude, ft")
     ax3.set_xlabel("temp")
     ax3.plot(harbor_data["temp_up"], harbor_data["alt_up"], "b-")
-    ax3.set_xlim(-40, 80)
+    a#x3.set_xlim(-40, 80)
+    ax3.set_xticks(np.arange(-40, 100, 20))
 
     ax4.set_xlabel("temp")
     ax4.plot(harbor_data["temp_down"], harbor_data["alt_down"], "b-")
-    ax4.set_xlim(-60, 120)
+    #ax4.set_xlim(-60, 120)
+    ax4.set_xticks(np.arange(-60, 120, 20))
     # ax4.invert_yaxis()
 
+    plt.show()  # display plot
+
+    """
+    Bellow is my attempt to fix graph with correct values
+    """
     # ax3.set_ylabel("Altitude, ft")
     # ax3.set_xlabel("Temperature, F")
     # ax3.plot(harbor_data["temp_up"], harbor_data["alt_up"], "b-")
-    ax3.set_xticks(np.arange(-40, 100, 20))
-
+    #ax3.set_xticks(np.arange(-40, 100, 20))
+    #
     # ax4.set_xlabel("Temperature, F")
     # ax4.plot(harbor_data["temp_down"], harbor_data["alt_down"], "b-")
-    ax4.set_xticks(np.arange(-60, 120, 20))
-
-
-    plt.show()  # display plot
+    #ax4.set_xticks(np.arange(-60, 120, 20))
+    #
+    #plt.show()  # display plot
 
 
 def main():
