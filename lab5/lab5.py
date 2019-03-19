0e0d0f0350d1a9b1f7a699555bbaf6b369a4aed4
Halofan240 <a.pytlik@comcast.net>
2019-03-18 21:31:30 -0600

    Lab 5:    Task 1: Finish    Task 2: WIP

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -3,7 +3,6 @@ from math import sin, cos
 import argparse
 
 
-
 def set_scene(data):
     """
     Set Vpython Scene
@@ -25,7 +24,7 @@ def motion_no_drag(data):
     Create animation for projectile motion with no dragging force
     """
     ball_nd = sphere(pos=vector(-25, data['init_height'], 0),
-                        radius=1, color=color.cyan, make_trail=True)
+                     radius=1, color=color.cyan, make_trail=True)
     # Follow the movement of the ball
     scene.camera.follow(ball_nd)
     # Set initial velocity & position
@@ -42,32 +41,43 @@ def motion_drag(data):
 
 def main():
     """
+    PS G:> python .\lab5\lab5.py --help
     """
     # 1) Parse the arguments
+    parser = argparse.ArgumentParser(description='Projectile Motion Demo')
+
+    parser.add_argument('--velocity', help='--velocity 20', required=True)
+    parser.add_argument('--angle', help='--angle 45', required=True)
+    parser.add_argument('--height', nargs='?', default=1.2, help='--height 1.2')
+
+    args = parser.parse_args()
 
     # Set Variables
-    data = {}       # empty dictionary for all data and variables
-#    data['init_height'] = args.height   # y-axis
-#    data['init_velocity'] = args.velocity  # m/s
-#    data['theta'] = args.angle       # degrees
+    data = {}  # empty dictionary for all data and variables
+    data['init_height'] = args.height  # y-axis
+    data['init_velocity'] = args.velocity  # m/s
+    data['theta'] = args.angle  # degrees
+
     # Constants
     data['rho'] = 1.225  # kg/m^3
-    data['Cd'] = 0.5    # coefficient friction
-    data['deltat'] = 0.005
+    data['Cd'] = 0.5  # coefficient friction
+    data['delta'] = 0.005
     data['gravity'] = -9.8  # m/s^2
 
     data['ball_mass'] = 0.145  # kg
     data['ball_radius'] = 0.075  # meters
-    data['ball_area'] = pi * data['ball_radius']**2
+    data['ball_area'] = pi * data['ball_radius'] ** 2
     data['alpha'] = data['rho'] * data['Cd'] * data['ball_area'] / 2.0
     data['beta'] = data['alpha'] / data['ball_mass']
     # Set Scene
+
+
 #    set_scene(data)
-    # 2) No Drag Animation
+# 2) No Drag Animation
 #    motion_no_drag(data)
-    # 3) Drag Animation
+# 3) Drag Animation
 #     motion_drag(data)
-    # 4) Plot Information: extra credit
+# 4) Plot Information: extra credit
 #     plot_data(data)
 
 
