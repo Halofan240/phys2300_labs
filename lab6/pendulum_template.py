ee21741343efa3a7311f25ed03facb754342566f
Halofan240 <a.pytlik@comcast.net>
2019-03-31 10:12:07 -0600

    Lab 6:    Task 1: Finish    Task 2: Finish    Task 3: Finish    Task 4: Finish    Comments: Finish    Code Cleanup: Finish

--- lab6/pendulum_template.py
+++ lab6/pendulum_template.py
@@ -16,7 +16,7 @@ steps_per_frame = 10
 def set_scene():
     """
     Set Vpython Scene
-    :return: Returns the pendulum to be change
+    :return: Returns the bob and rod to be change
     """
     scene.title = "Assignment 6: Pendulum"
     scene.width = 800
@@ -28,7 +28,9 @@ def set_scene():
     scene.forward = vector(0, .3, -2)
     scene.x = -1
 
-    # Set background: floor, table, etc
+    # Set background: Ceiling, bob, and rod
+
+    # Pivot point of the pendulum
     pivot = vector(0, 0, 0)
 
     # Creates the ceiling
@@ -36,7 +38,7 @@ def set_scene():
                   size=vector(.1, .025, .1),
                   color=color.gray(.5))
 
-    # Creates the ball
+    # Creates the bob
     bob = sphere(pos=pivot - vector(0, l, 0),
                  radius=R,
                  color=color.red)
@@ -53,7 +55,10 @@ def set_scene():
 
 def f(r, t):
     """
-    Pendulum
+    Sets the angular position and acceleration
+    :param r: holds the current vector state
+    :param t: the current time
+    :return: returns the theta and acceleration
     """
     theta = r[0]
     omega = r[1]
@@ -63,6 +68,13 @@ def f(r, t):
 
 
 def rk4(r, t, dt):
+    """
+    rung kutta 4 function
+    :param r: Contains the current vector state
+    :param t: Current time
+    :param dt: Amount ot increment current time
+    :return: returns the results of th rung kutta 4
+    """
     k1 = dt * f(r, t)
     k2 = dt * f(r + 0.5 * k1, t + 0.5 * dt)
     k3 = dt * f(r + 0.5 * k2, t + 0.5 * dt)
@@ -72,10 +84,10 @@ def rk4(r, t, dt):
 
 def main():
     # Set up initial values
-    r = np.array([np.pi * 179 / 180, 0], float)
-    # r = np.array([np.pi*79 / 180, 0], float)
-    # r = np.array([np.pi*20/180, 0], float)
-    theta_title = r[0] * 180 / np.pi
+    r = np.array([np.pi * 179 / 180, 0], float)  # Theta = 179
+    # r = np.array([np.pi*79 / 180, 0], float)  # Theta = 79
+    # r = np.array([np.pi*20/180, 0], float)  # Theta = 20
+    theta_title = r[0] * 180 / np.pi  # Title of current plot theta
 
     # Initial x and y
     x = l * np.sin(r[0])
@@ -93,15 +105,17 @@ def main():
     t = 0
 
     while t < 25:
-        # Use the 4'th order Runga-Kutta approximation
+        # Sets the framerate
         rate(framerate)
 
+        # Use the 4'th order Runga-Kutta approximation
         r, t = rk4(r, t, dt)
 
         # Update positions
         x = l * np.sin(r[0])
         y = rod.pos.y - l * np.cos(r[0])
 
+        # Stores the position and time for plot
         xpoints.append(x)
         tpoints.append(t)
 
@@ -111,7 +125,7 @@ def main():
         rod.axis = bob.pos - rod.pos
         t = t + dt
 
-    # Task 1
+    # Plots the pendulum
     plt.plot(tpoints, xpoints)
     plt.title("Theta = %d" % theta_title)
     plt.xlabel("t")
