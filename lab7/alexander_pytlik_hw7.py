877ac87baf2db58f306f391b610a4f6b8da96795
Halofan240 <a.pytlik@comcast.net>
2019-04-06 14:09:35 -0600

    Lab 7:    Task 1: Initial file

---
+++ lab7/alexander_pytlik_hw7.py
@@ -0,0 +1,12 @@
+# Declare arras to hold the masses, initial positions, and initial velocities of the particles
+# Determine the initial values (get from user)
+# Determine the timestep, and how long the user wants the simulation to run
+# for each timestep:
+#     for each particle:
+#         for all the other particles
+#         * Compute the acceleration due to gravity
+#         * Use the acceleration to compute the change in velocity
+#         * Use the velocity to compute the change in position
+#         * Add the changes in acceleration, velocity and position to the particle's current values
+#         * Report
+# End the program when simulation is complete
